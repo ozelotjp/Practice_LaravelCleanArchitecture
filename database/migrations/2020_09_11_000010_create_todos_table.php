f9d82797ce9489b24e17c9fbfe3370e34d1c2074
ozelotjp <contact@ozelot.app>
2020-09-11 13:15:25 +0900

    chore: TodoModelを追加

---
+++ database/migrations/2020_09_11_000010_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string("text");
+            $table->dateTime("done_at")->nullable();
+            $table->dateTime("created_at")->useCurrent();
+            $table->dateTime("updated_at")->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
