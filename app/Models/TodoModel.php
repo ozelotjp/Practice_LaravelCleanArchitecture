612d082be4401530a992025b12fb4c9f40b01cca
ozelotjp <contact@ozelot.app>
2020-09-11 13:33:32 +0900

    deps: laravel-ide-helperとlaravel-debugbarを追加

--- app/Models/TodoModel.php
+++ app/Models/TodoModel.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\TodoModel
+ *
+ * @property int $id
+ * @property string $text
+ * @property string|null $done_at
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel whereDoneAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel whereText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TodoModel whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class TodoModel extends Model
 {
     use HasFactory;
